45a890f332e59799289d52c1dc13df4ae8c581ad
Jayaraja S.K <63498702+Jayaraja-SK@users.noreply.github.com>
2021-08-19 08:56:28 +0530

    Add files via upload

---
+++ Selection_Sort.py
@@ -0,0 +1,17 @@
+def Selection_Sort(L):
+    n=len(L)
+    
+    for i in range(n-1):
+        for j in range(i+1,n):
+            if L[i]>L[j]:
+                L[i],L[j]=L[j],L[i]
+    return L
+
+
+L=list(map(int,input().split())) #INPUT ELEMENTS OF LIST AS - 3 2 1 4 5
+
+sorted_L=Selection_Sort(L)
+
+print(*sorted_L)
+
+       
