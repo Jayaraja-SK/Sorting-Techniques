1d3b52bf60a9029b219b1d5f6285834dbe85193c
Jayaraja S.K <63498702+Jayaraja-SK@users.noreply.github.com>
2021-08-19 09:13:50 +0530

    Add files via upload

---
+++ Merge_Sort.py
@@ -0,0 +1,34 @@
+def Merge_Sort(L): #TIME COMPLEXITY IS O(N*Log(N))
+    n=len(L)
+    
+    if n<2:
+        return L[:]
+    else:
+        mid=n//2
+        return merge(Merge_Sort(L[:mid]),Merge_Sort(L[mid:]))
+
+def merge(L1,L2):
+    i,j=0,0
+    res=[]
+
+    while(i<len(L1) and j<len(L2)):
+        if L1[i]<L2[j]:
+            res.append(L1[i])
+            i=i+1
+        else:
+            res.append(L2[j])
+            j=j+1
+
+    if i<len(L1):
+        res.extend(L1[i:])
+    else:
+        res.extend(L2[j:])
+
+    return res
+
+
+L=list(map(int,input().split())) #INPUT LIST AS - 3 2 1 4 5
+
+sorted_L=Merge_Sort(L)
+
+print(*sorted_L)
